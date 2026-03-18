4a651d0b1bc3577b11860204af6c2cb64801898c
Brandon Young <brandon.young@pharmalytica.io>
2026-03-18 10:08:17 -0400

    chore: bump version to v0.27.4

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 // This file is updated as part of the version bump process.
 // It provides the version string to compiled binaries where
 // package.json is not available at runtime.
-export const VERSION = "0.27.3";
+export const VERSION = "0.27.4";
